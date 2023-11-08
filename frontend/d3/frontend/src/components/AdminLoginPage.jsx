ac03434af6c10a7fd6d07ed127ef7664697ba76b
MaxLyu254 <maxlyu254@gmail.com>
2023-11-08 16:25:25 -0500

    added incomplete admin login page

---
+++ frontend/d3/frontend/src/components/AdminLoginPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../Plain.css';
+
+function AdminLoginPage() {
+    return (
+        <body>
+            <nav className="navbar">
+                <div className="navbar__container">
+                    <a href="#home" id="navbar__logo">Admin Login</a>
+                </div>
+            </nav>
+            <div className="option__frame">
+                <p className="option__text"><a href="seasons.html">Login</a></p>
+            </div>
+        </body>
+    );
+}
