99689caf9263530a7e97a6ead23f022248699a89
Yin Hao <yinhao@Yins-MacBook-Pro.local>
2023-11-09 16:37:26 -0500

    [+] modify the card

---
+++ frontend/d3/frontend/src/components/Card.jsx
@@ -0,0 +1,16 @@
+const Card = () => {
+    return(
+        <div className = "card" style="width: 18rem;">
+        <img src="..." className="card-img" alt="...">
+        <div className="card-body">
+            <h5 className="card-title"> Card title</h5>
+            <p className="card-text"> name of the audio</p>
+            <a href="#" className="edit"> This is a edit page</a>
+
+        </div>
+        </img>
+        </div> 
+        
+    )
+}
+export default Card;
