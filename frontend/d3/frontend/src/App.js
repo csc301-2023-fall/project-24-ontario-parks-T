f891c5c2b678dba32048d9b1e7392ef0ecef70fd
yi1z <yilz.zhang@mail.utoronto.ca>
2023-11-09 16:31:42 -0500

    Added English version of the websites, minor changes

--- frontend/d3/frontend/src/App.js
+++ frontend/d3/frontend/src/App.js
@@ -4,16 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
 
-import UserPageMainFrame from './components/UserMain';
-import AudioPlayer from './components/AudioPlayer';
+import EngUserPageMainFrame from './components/EngUserMain';
+import EngAudioPlayer from './components/EngAudioPlayer';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/components/Card/" element={<AudioPlayer />}/>
-          <Route path="/play/:location_id" element={<AudioPlayer />} />
+        <Route path="/english" >
+          <Route path="play/:location_id" element={<EngAudioPlayer />} />
+        </Route>
 
+        <Route path="/french" element={<></>} >
+          <Route path="play/:location_id" element={<></>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
