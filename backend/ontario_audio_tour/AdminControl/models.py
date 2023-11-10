de89c45fbf8a272a299651ac2522078d5b2da3b7
yi1z <yilz.zhang@mail.utoronto.ca>
2023-11-10 17:18:09 -0500

    Add connections to MySQL database, modified models

--- backend/ontario_audio_tour/AdminControl/models.py
+++ backend/ontario_audio_tour/AdminControl/models.py
@@ -4,20 +4,23 @@ from django.db import models
 
 
 class Audio(models.Model):
-    media_id = models.AutoField(primary_key=True)
+    media_id = models.AutoField(primary_key=True, unique=True, default=0)
     name = models.CharField(max_length=100, unique=True)
-    audio = models.FileField(upload_to="music/")
-    type = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
-    image = models.ImageField(upload_to="images/")
-    season = models.CharField(max_length=100)
-    year = models.IntegerField()
-    month = models.IntegerField()
-    day = models.IntegerField()
-    longitude = models.FloatField()
-    latitude = models.FloatField()
+    link = models.FileField(upload_to="audio/", default="")
+    type = models.CharField(max_length=100, default="audio")
+    description = models.CharField(max_length=500, default="")
+    image = models.ImageField(upload_to="images/", default="")
+    season = models.CharField(max_length=100, default="Spring")
+    year = models.IntegerField(null=True)
+    month = models.IntegerField(null=True)
+    day = models.IntegerField(null=True)
+    longitude = models.FloatField(null=True)
+    latitude = models.FloatField(null=True)
     # uploader = models.CharField(max_length=100)
     # last_modified_time = models.DateTimeField()
+
+    class Meta:
+        db_table = "medias"
 
     def __str__(self):
         return self.name
@@ -41,9 +44,27 @@ class Image(models.Model):
 
 
 class Location(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    audios = models.ManyToManyField(Audio)
-    images = models.ManyToManyField(Image)
+    Location_id = models.AutoField(primary_key=True, unique=True)
+    location_name = models.CharField(max_length=100, unique=True)
+    location_address = models.CharField(max_length=100, unique=True)
+    location_city = models.CharField(max_length=100, unique=True)
+    location_province = models.CharField(max_length=100, unique=True)
+    location_postal_code = models.CharField(max_length=100, unique=True)
+    location_longitude = models.FloatField(null=True)
+    location_latitude = models.FloatField(null=True)
+
+    class Meta:
+        db_table = "locations"
 
     def __str__(self):
         return self.name
+    
+
+class locations_have_medias(models.Model):
+    pair_id = models.AutoField(primary_key=True, unique=True)
+    Location_id = models.ForeignKey(Location, on_delete=models.CASCADE)
+    media_id = models.ForeignKey(Audio, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "locations_have_medias"
+        unique_together = (("Location_id", "media_id"),)
