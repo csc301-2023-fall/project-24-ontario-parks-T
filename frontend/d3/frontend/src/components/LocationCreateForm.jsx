def907c0379f5f6a4ae5535bcf088db860c89825
Yin Hao <yinhao@yins-mbp.home>
2023-11-19 16:03:22 -0500

    [+] make it more reasonable

--- frontend/d3/frontend/src/components/LocationCreateForm.jsx
+++ frontend/d3/frontend/src/components/LocationCreateForm.jsx
@@ -3,66 +3,72 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import AdminMain from "./AdminMain";
 
-
-const LocationCreationForm = () =>{
+const LocationCreationForm = () => {
     const [locationdata, setLocationData] = useState({
-        location_id : '',
-        location_name : '',
-        location_address : '',
-        location_city : '',
-        location_province : '',
-        location_postal_code : '',
-        longitude : '',
-        latitude : '',
-      });
-    
-
+        location_id: '',
+        location_name: '',
+        location_address: '',
+        location_city: '',
+        location_province: '',
+        location_postal_code: '',
+        longitude: '',
+        latitude: '',
+    });
 
     const handleChange = (e) => {
         setLocationData({
-          ...locationdata,
-          [e.target.name]: e.target.value
+            ...locationdata,
+            [e.target.name]: e.target.value
         });
-      };
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-          const response = await axios.post('http://localhost:8000/AdminControl/api/location/', locationdata);
-          console.log(response.data);
+            const response = await axios.post('http://localhost:8000/AdminControl/api/location/', locationdata);
+            console.log(response.data);
         } catch (error) {
-          console.error(error);
+            console.error(error);
         }
-      };
+    };
 
-    return(
+    return (
         <div>
             <AdminMain />
-            <form class="card" onSubmit={handleSubmit}>
-                <div class="form-group">
+            <form className="card" onSubmit={handleSubmit}>
+                <div className="form-group">
                     <label>Location Name</label>
-                    <input name="name" onChange={handleChange} placeholder="Name" />
+                    <input name="location_name" onChange={handleChange} placeholder="Location Name" />
                 </div>
-                <div class="form-group">
-                    <label>Location Description</label>
-                    <input name="description" onChange={handleChange} placeholder="description" />
+
+                <div className="form-group">
+                    <label>Location Address</label>
+                    <input name="location_address" onChange={handleChange} placeholder="Address" />
                 </div>
-                <div class="form-group">
-                    <label>Location Name French</label>
-                    <input name="name_french" onChange={handleChange} placeholder="Name" />
+                <div className="form-group">
+                    <label>Location City</label>
+                    <input name="location_city" onChange={handleChange} placeholder="City" />
                 </div>
-                <div class="form-group">
-                    <label>Location Description French</label>
-                    <input name="description_french" onChange={handleChange} placeholder="description french" />
+                <div className="form-group">
+                    <label>Location Province</label>
+                    <input name="location_province" onChange={handleChange} placeholder="Province" />
                 </div>
-                <div class="form-group">
-                    <label>Link To Location</label>
-                    <input name="link" onChange={handleChange} placeholder="Link" />
+                <div className="form-group">
+                    <label>Location Postal Code</label>
+                    <input name="location_postal_code" onChange={handleChange} placeholder="Postal Code" />
                 </div>
-                <button class="btn btn-primary" type="submit">Submit</button>
+                <div className="form-group">
+                    <label>Longitude</label>
+                    <input name="longitude" onChange={handleChange} placeholder="Longitude" />
+                </div>
+                <div className="form-group">
+                    <label>Latitude</label>
+                    <input name="latitude" onChange={handleChange} placeholder="Latitude" />
+                </div>
+                <button className="btn btn-primary" type="submit">Submit</button>
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default LocationCreationForm;
