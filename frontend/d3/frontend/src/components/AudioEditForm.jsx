88c241798c459cb322494fda8559d5139806b7ad
MacroSony <jameszh.wang@mail.utoronto.ca>
2023-11-19 16:20:38 -0500

    format edit form

--- frontend/d3/frontend/src/components/AudioEditForm.jsx
+++ frontend/d3/frontend/src/components/AudioEditForm.jsx
@@ -67,29 +67,29 @@ const AudioEditForm = ({}) =>{
             <AdminMain />
             <div style={containerStyle}>
             <form className="card" onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label>Audio Name</label>
-                    <input name="name" onChange={handleChange} value={audioData.name} />
+                <div className="form-group row m-2">
+                    <label className="col-4 col-form-label">Audio Name</label>
+                    <input className="form-control col-8" name="name" onChange={handleChange} value={audioData.name} />
                 </div>
-                <div className="form-group">
-                    <label>Audio Description</label>
-                    <input name="description" onChange={handleChange}  value={audioData.description}  />
+                <div className="form-group row m-2">
+                    <label className="col-4 col-form-label">Audio Description</label>
+                    <textarea className="form-control col-8" rows="5" name="description" onChange={handleChange}  value={audioData.description}  />
                 </div>
-                <div className="form-group">
-                    <label>Audio Name French</label>
-                    <input name="name_french" onChange={handleChange}  value={audioData.name_french}  />
+                <div className="form-group row m-2">
+                    <label className="col-4 col-form-label">Audio Name French</label>
+                    <input className="form-control col-8" name="name_french" onChange={handleChange}  value={audioData.name_french}  />
                 </div>
-                <div className="form-group">
-                    <label>Audio Description French</label>
-                    <input name="description_french" onChange={handleChange}  value={audioData.description_french}  />
+                <div className="form-group row m-2">
+                    <label className="col-4 col-form-label">Audio Description French</label>
+                    <textarea className="form-control col-8" rows="5" name="description_french" onChange={handleChange}  value={audioData.description_french}  />
                 </div>
-                <div className="form-group">
-                    <label>Link To Audio</label>
-                    <input name="link" onChange={handleChange}  value={audioData.link}  />
+                <div className="form-group row m-2">
+                    <label className="col-4 col-form-label">Link To Audio</label>
+                    <input className="form-control col-8" name="link" onChange={handleChange}  value={audioData.link}  />
                 </div>
-                <div className="form-group">
-                    <label>Location</label>
-                    <select name="location_name" value={audioData.location.location_name} onChange={handleLocationChange}>
+                <div className="form-group row m-2">
+                    <label className="col-4 col-form-label">Location</label>
+                    <select className="form-control col-8" name="location_name" value={audioData.location.location_name} onChange={handleLocationChange}>
                         {locations.map((location) => (
                         <option key={location.location_name} value={location.location_name}>
                             {location.location_name}
