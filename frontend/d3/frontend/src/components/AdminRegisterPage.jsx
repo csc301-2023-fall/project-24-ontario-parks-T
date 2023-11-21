adaa5f00c2ca3ccb0ac6f6ed6cf0072157a6dac0
Yuhan-Ut <brandon.fu@mail.utoronto.ca>
2023-11-20 20:03:37 -0500

    register

---
+++ frontend/d3/frontend/src/components/AdminRegisterPage.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+import { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import AdminPageMainFrame from "./AdminMain";
+import LoginInput from "./LoginInput";
+
+const AdminRegisterPage = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const navigate = useNavigate();
+
+    const Register = (username, password, email,navigate) => {
+        // check if the username and password is valid
+        var bad = false;
+        const username_not = document.getElementById("Username_notification");
+        const pwd_not = document.getElementById("Password_notification");
+        const email_not = document.getElementById("Email_notification");
+
+        if (username === "") {
+            username_not.innerHTML = "Please enter your username";
+            bad = true;
+        }
+        else (
+            username_not.innerHTML = ""
+        )
+
+        if (password === "") {
+            pwd_not.innerHTML = "Please enter your password";
+            bad = true;
+        }
+        else (
+            pwd_not.innerHTML = ""
+        )
+
+        if (email === "") {
+            email_not.innerHTML = "Please enter your email";
+            bad = true;
+        }
+        else (
+            email_not.innerHTML = ""
+        )
+
+        if (bad) {
+            return;
+        }
+        
+        username_not.innerHTML = "";
+        pwd_not.innerHTML = "";
+        email_not.innerHTML = "";
+
+        var data = new FormData();
+        data.append("username", username);
+        data.append("password", password);
+        data.append("email", email);
+    
+        fetch("http://localhost:8000/AdminControl/register/", {
+            method: "POST",
+            body: data,
+            
+        })
+        .then(respones => respones.json())
+        .then(data => {
+            if (data.success) {
+                navigate("/admin/login");
+            } else {
+                console.error("Registration failed:", data.error);
+            }
+        })
+        .catch(error => console.error("Error:", error))
+    }
+        
+    
+
+    return (
+        <>
+            <AdminPageMainFrame />
+
+            <div className="container">
+                <div className="row" style={{height: 4 + "rem"}}>
+                    
+                </div>
+
+            </div>
+
+            <div className="container-fluid">
+
+            <div className="row">
+
+                <div className="col">
+
+                </div>
+
+                <div className="col-5">
+                    <div className="card p-5" >
+                            <form>
+                                <div className="mb-3">
+                                        <LoginInput input_lable_value="Username" 
+                                        input_value={username} 
+                                        update={setUsername} 
+                                        placeholder_value="" 
+                                        type_value="text" 
+                                        is_required={true} />
+                                </div>
+                                <div className="mb-3">
+                                        <LoginInput input_lable_value="Password" 
+                                        input_value={password} 
+                                        update={setPassword} 
+                                        placeholder_value="" 
+                                        type_value="password" 
+                                        is_required={true} />
+                                </div>
+                                <div className="mb-3">
+                                        <LoginInput input_lable_value="Email" 
+                                        input_value={email} 
+                                        update={setEmail} 
+                                        placeholder_value="" 
+                                        type_value="email" 
+                                        is_required={true} />
+                                </div>
+                                <div className="mb-3">
+                                    <button type="button" className="btn btn-secondary" style={{marginLeft: 0 + "px"}}
+                                    onClick={() => {
+                                        Register(username, password,email, navigate);
+                                    }}
+                                    >
+                                        Register
+                                    </button>
+                                </div>
+                            </form>
+                    </div>
+                </div>
+
+                <div className="col">
+
+                </div>
+            </div>
+
+            </div>
+        </>
+    );
+}
+
+export default AdminRegisterPage;
