31991c184d5424e662650989910ad0a579fddd00
JacqJingyuH <hjyjacquelyn29@gmail.com>
2023-11-21 03:27:55 -0500

    minor fix

--- frontend/d3/frontend/src/App.js
+++ frontend/d3/frontend/src/App.js
@@ -22,7 +22,7 @@ import AdminLoginPage from './components/AdminLoginPage';
 import LocationEditForm from './components/LocationEditForm';
 import LogoutPage from "./components/Logout";
 import AdminRegisterPage from "./components/AdminRegisterPage";
-import { backendAPI } from "./components/config";
+import { BACKENDHOST } from "./components/config";
 
 function App() {
   useEffect(() => {
@@ -31,7 +31,7 @@ function App() {
         const data = {
           refresh: localStorage.getItem('refresh'),
         };
-        const response = await axios.post(`${backendAPI}AdminControl/api/token/refresh/`, data);
+        const response = await axios.post(`${BACKENDHOST}AdminControl/api/token/refresh/`, data);
 
         localStorage.setItem('token', response.data.access);
 
