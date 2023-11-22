f391d71fecd83554844ab82b9b51e736f5a87c02
Yuhan-Ut <brandon.fu@mail.utoronto.ca>
2023-11-21 22:00:34 -0500

    last min fix

--- frontend/d3/frontend/src/components/LocationCreateForm.jsx
+++ frontend/d3/frontend/src/components/LocationCreateForm.jsx
@@ -43,50 +43,61 @@ const LocationCreationForm = () => {
     return (
         <div>
             <AdminMain />
-            <form className="card" onSubmit={handleSubmit}>
-                {/* Form fields */}
-                <div className="form-group">
-                    <label>Location Name</label>
-                    <input name="location_name" type="text" onChange={handleChange} placeholder="Location Name" />
-                </div>
+            <div style={containerStyle}>
+                <form className="card" onSubmit={handleSubmit}>
+                    {/* Form fields */}
+                    <div className="form-group">
+                        <label>Location Name</label>
+                        <input name="location_name" type="text" onChange={handleChange} placeholder="Location Name" />
+                    </div>
 
-                <div className="form-group">
-                    <label>Location Address</label>
-                    <input name="location_address" type="text" onChange={handleChange} placeholder="Address" />
-                </div>
+                    <div className="form-group">
+                        <label>Location Address</label>
+                        <input name="location_address" type="text" onChange={handleChange} placeholder="Address" />
+                    </div>
 
-                <div className="form-group">
-                    <label>Location City</label>
-                    <input name="location_city" type="text" onChange={handleChange} placeholder="City" />
-                </div>
+                    <div className="form-group">
+                        <label>Location City</label>
+                        <input name="location_city" type="text" onChange={handleChange} placeholder="City" />
+                    </div>
 
-                <div className="form-group">
-                    <label>Location Province</label>
-                    <input name="location_province" type="text" onChange={handleChange} placeholder="Province" />
-                </div>
+                    <div className="form-group">
+                        <label>Location Province</label>
+                        <input name="location_province" type="text" onChange={handleChange} placeholder="Province" />
+                    </div>
 
-                <div className="form-group">
-                    <label>Location Postal Code</label>
-                    <input name="location_postal_code" type="text" onChange={handleChange} placeholder="Postal Code" />
-                </div>
+                    <div className="form-group">
+                        <label>Location Postal Code</label>
+                        <input name="location_postal_code" type="text" onChange={handleChange} placeholder="Postal Code" />
+                    </div>
 
-                <div className="form-group">
-                    <label>Longitude</label>
-                    <input name="longitude" type="text" onChange={handleChange} placeholder="Longitude" />
-                </div>
+                    <div className="form-group">
+                        <label>Longitude</label>
+                        <input name="longitude" type="text" onChange={handleChange} placeholder="Longitude" />
+                    </div>
 
-                <div className="form-group">
-                    <label>Latitude</label>
-                    <input name="latitude" type="text" onChange={handleChange} placeholder="Latitude" />
-                </div>
+                    <div className="form-group">
+                        <label>Latitude</label>
+                        <input name="latitude" type="text" onChange={handleChange} placeholder="Latitude" />
+                    </div>
 
-                <button className="btn btn-primary" type="submit">Submit</button>
-            </form>
-            {/* Feedback Messages */}
-            {submissionStatus === 'success' && <div className="alert alert-success">Location created successfully!</div>}
-            {submissionStatus === 'error' && <div className="alert alert-danger">Error creating location. Please try again.</div>}
+                    <button className="btn btn-primary" type="submit">Submit</button>
+                </form>
+                {/* Feedback Messages */}
+                {submissionStatus === 'success' && <div className="alert alert-success">Location created successfully!</div>}
+                {submissionStatus === 'error' && <div className="alert alert-danger">Error creating location. Please try again.</div>}
+            </div>
         </div>
     );
+};
+const containerStyle = {
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    marginLeft: "5cm",
+    marginRight: "5cm",
+    padding: "1cm",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    overflowY: "auto",
 };
 
 export default LocationCreationForm;
