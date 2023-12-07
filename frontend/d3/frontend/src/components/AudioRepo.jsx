9edb25d6785d839168b83ecd4681388f3bfda6bb
yin0409 <yinhao@yins-mbp.home>
2023-12-07 15:12:06 -0500

    [+] add comment for developer

--- frontend/d3/frontend/src/components/AudioRepo.jsx
+++ frontend/d3/frontend/src/components/AudioRepo.jsx
@@ -4,10 +4,13 @@ import axios from "axios";
 import { NavLink } from 'react-router-dom';
 import { BACKENDHOST } from "./config";
 
+// Component to display a list of audio files
 const Audios = () => {
+    // State hooks for audio list and error message
     const [audios, setAudios] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
 
+    // Function to refresh the list of audios
     const refreshList = () => {
         axios
             .get(`${BACKENDHOST}AdminControl/api/audio/`, {
@@ -16,10 +19,12 @@ const Audios = () => {
                 }
             })
             .then((res) => {
+                // Update the audio list upon successful fetch
                 setAudios(res.data);
                 setErrorMessage('');
             })
             .catch((err) => {
+                // Handle errors, including redirecting if not logged in
                 if (err.response && err.response.status === 401) {
                     setErrorMessage('You are not logged in. Redirecting to login page...');
                     setTimeout(() => {
@@ -31,6 +36,7 @@ const Audios = () => {
             });
     };
 
+    // Function to handle deletion of an audio file
     const handleDelete = (name) => {
         axios
             .delete(`${BACKENDHOST}AdminControl/api/audio/${name}/`, {
@@ -39,33 +45,42 @@ const Audios = () => {
                 }
             })
             .then((res) => {
+                // Log the response and refresh the list after deletion
                 console.log("Delete response:", res.data);
                 refreshList();
             })
             .catch((err) => console.log(err));
     };
 
+    // useEffect hook to refresh the list on component mount
     useEffect(() => {
         refreshList();
     }, []);
 
+    // JSX to render the list of audios
     return (
         <div>
+            {/* Display any error messages */}
             <p>{errorMessage}</p>
+            {/* Map over the list of audios and display each one */}
             {audios.map((audio) => (
                 <div key={audio.media_id} className="card mb-3">
                     <div className="card-body">
+                        {/* Audio name */}
                         <h5 className="card-title">{audio.name}</h5>
                         <div className="d-flex justify-content-between align-items-center">
+                            {/* Audio name in French */}
                             <h6 className={`mr-2`}>
                                 {audio.name_french}
                             </h6>
                             <div>
+                                {/* Edit button */}
                                 <button className="btn btn-secondary ml-1 mr-2">
                                     <NavLink className="nav-link" to={`edit/${audio.name}`}>
                                         Edit
                                     </NavLink>
                                 </button>
+                                {/* Delete button */}
                                 <button className="btn btn-danger ml-1 mr-2"
                                     onClick={() => handleDelete(audio.name)}>
                                     Delete
@@ -77,14 +92,16 @@ const Audios = () => {
             ))}
         </div>
     );
-
 };
 
+// Main component for the audio repository
 const AudioRepo = () => {
     return (
         <div>
+            {/* Admin main frame component */}
             <AdminMain />
             <div className="list-container">
+                {/* Including the Audios component to display the list */}
                 <Audios />
             </div>
         </div>
