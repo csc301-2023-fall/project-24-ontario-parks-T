bd8222ac0dc9f7692bd85b8dafa1bcc40901e6f7
yin0409 <yinhao@yins-mbp.home>
2023-12-07 15:18:11 -0500

    [+]add comment for developer

--- frontend/d3/frontend/src/components/EngAudioPlayer.jsx
+++ frontend/d3/frontend/src/components/EngAudioPlayer.jsx
@@ -1,11 +1,9 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
 import { BACKENDHOST } from "./config";
-
 import EngUserPageMainFrame from "./EngUserMain";
 
-
+// Function to determine the current season based on the current month
 function getSeason() {
     let currentTime = new Date();
     let month = currentTime.getMonth();
@@ -25,64 +23,65 @@ function getSeason() {
     }
 }
 
-
+// Component to play an audio file
 const EngAudioPlay = () => {
     const host = `${BACKENDHOST}AdminControl`
 
-    // get the audio id from scanned
+    // Retrieving the location name from the URL parameters
     const location_name = useParams()["location_name"];
 
+    // State hooks for managing audio data
     const [audioPath, setAudioPath] = useState("");
     const [audioName, setAudioName] = useState("Default Name");
     const [audioDescription, setAudioDescription] = useState("This is a default example description.");
     const [audioImage, setAudioImage] = useState("Ontario-Parks-Title.jpg");
     const [currSeason, setCurrSeason] = useState("Spring");
 
-    // get the audio data
-    fetch(host + "/api/location/" + location_name + "/audio_list/", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }
-    ).then((response) => {
-        if (response.ok) {
-            return response.json();
-        }
-        else {
-            console.log("Error: " + response.status);
-        }
-    }
-    ).then((data) => {
-        const season = getSeason();
-
-        // get the audio at the current season
-        for (let i = 0; i < data.length; i++) {
-            if (data[i]["season"] == season) {
-                setAudioPath(data[i]["link"]);
-                setAudioName(data[i]["name"]);
-                setAudioDescription(data[i]["description"]);
-                setAudioImage(data[i]["image"]);
-                setCurrSeason(season);
-                break;
+    // Fetching audio data from the backend
+    useEffect(() => {
+        fetch(host + "/api/location/" + location_name + "/audio_list/", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
             }
-        }
-    })
+            else {
+                console.log("Error: " + response.status);
+            }
+        })
+        .then((data) => {
+            const season = getSeason();
 
-    // return the audio player
+            // Setting the audio data for the current season
+            for (let i = 0; i < data.length; i++) {
+                if (data[i]["season"] === season) {
+                    setAudioPath(data[i]["link"]);
+                    setAudioName(data[i]["name"]);
+                    setAudioDescription(data[i]["description"]);
+                    setAudioImage(data[i]["image"]);
+                    setCurrSeason(season);
+                    break;
+                }
+            }
+        });
+    }, [location_name]);
+
+    // JSX for rendering the audio player
     return (
         <>
             <EngUserPageMainFrame season={currSeason} location_name={location_name} />
 
             <div className="container-fluid">
-
-                <div className="mx-5" style={{height: 5 + "rem"}}>
-
-                </div>
+                <div className="mx-5" style={{height: 5 + "rem"}}></div>
 
                 <div className="row">
                     <div className="col-3 d-sm-none"></div>
                     <div className="container col-md-auto">
+                        {/* Card displaying the audio information */}
                         <div className="card mx-auto" style={{width: 24 + "rem"}}>
                             <img className="mx-auto d-block img-fluid rounded" src={audioImage} alt="Audio Image"/>
                             <div className="card-body">
@@ -92,19 +91,12 @@ const EngAudioPlay = () => {
                                 </p>
                                 
                                 <div className="text-center">
+                                    {/* Audio player */}
                                     <audio id="dynamicAudio" style={{width: 90 + "%"}} src={audioPath} controls>
-                                    Your browser does not support the audio element.
+                                        Your browser does not support the audio element.
                                     </audio>
                                 </div>
-
-                                {/* <div className="text-center">
-                                    <audio id="dynamicAudio" style={{width: 90 + "%"}} controls>
-                                        <source src={audioPath} type="audio/mpeg"/>
-                                    </audio>
-                                </div> */}
-
                             </div>
-
                         </div>
                     </div>
                     <div className="col-3 d-sm-none"></div>
