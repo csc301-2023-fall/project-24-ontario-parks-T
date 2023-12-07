f4e2525f3c060eb91b8c04f7ad0e313682babe56
yin0409 <yinhao@yins-mbp.home>
2023-12-07 15:09:27 -0500

    [+] add comment for developer

--- frontend/d3/frontend/src/components/AudioPlayer.jsx
+++ frontend/d3/frontend/src/components/AudioPlayer.jsx
@@ -1,53 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
 import { BACKENDHOST } from "./config";
-
 import UserPageMainFrame from "./UserMain";
 
-
+// Component for playing audio
 const AudioPlay = () => {
+    // Base URL for backend host
     const host = BACKENDHOST;
 
-    // get the audio id from scanned
+    // Retrieving the location ID from the URL parameters
     var location_id = useParams()["location_id"];
 
+    // State hooks for managing audio path, name, image, and current season
     const [audioPath, setAudioPath] = useState("");
     const [audioName, setAudioName] = useState("");
     const [audioImage, setAudioImage] = useState("");
     const [currSeason, setCurrSeason] = useState("Spring");
 
+    // Logging the audio ID for debugging purposes
     console.log("audioID: " + location_id);
 
-    // get the audio data
-    // fetch(host + "/location/get/" + location_id, {
-    //     method: "GET",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     },
-    // }
-    // ).then((response) => {
-    //     if (response.ok) {
-    //         return response.json();
-    //     }
-    //     else {
-    //         throw new Error("Failed to get audio data");
-    //     }
-    // }
-    // ).then((data) => {
-    //     setAudioPath(data["path"]);
-    //     setAudioName(data["name"]);
-    //     setAudioImage(data["image"]);
-    // })
-
-    // return the audio player
+    // JSX for rendering the audio play page
     return (
         <>
+            {/* Including the main frame for the user page */}
             <UserPageMainFrame season={currSeason} />
+            {/* Layout for audio player */}
             <div className="row">
                 <div className="col-3"></div>
                 <div className="container text-center col-md-auto">
+                    {/* Image for the audio (currently without src and alt attributes set) */}
                     <img src="" alt="" />
+                    {/* Audio player element */}
                     <audio id="dynamicAudio" controls>
                         <source src={audioPath} type="audio/mpeg"/>
                     </audio>
