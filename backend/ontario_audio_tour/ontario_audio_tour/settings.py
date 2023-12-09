36bd7f307ab85d34122bcaec32b65ebb77ee844a
JacqJingyuH <hjyjacquelyn29@gmail.com>
2023-12-09 16:40:31 -0500

    updat cors allowed origins

--- backend/ontario_audio_tour/ontario_audio_tour/settings.py
+++ backend/ontario_audio_tour/ontario_audio_tour/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 
 # yilz: add corsheaders
 CORS_ALLOWED_ORIGINS = [
-    "http://3.99.190.60:3000",
+    "http://3.99.190.60:3000", "http://127.0.0.1:3000", "http://localhost:3000"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
